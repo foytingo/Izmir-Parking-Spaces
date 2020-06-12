14f03bb7417e9044cbd4f3fb03dd54cdf621d54d
Murat Baykor <foytingo@gmail>
2020-06-12 12:38:50 +0300

    Initial Commit

---
+++ Izmir Parking Spaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Izmir Parking Spaces
+//
+//  Created by Murat Baykor on 12.06.2020.
+//  Copyright © 2020 Murat Baykor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
